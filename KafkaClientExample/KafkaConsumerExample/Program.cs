1160118b8b843620919d5ca6018dc9b8e16aef07
Turkmenoglu, Ismail <Ismail.Turkmenoglu@adesso.com.tr>
2020-06-26 13:25:08 +0300

    first commit

---
+++ KafkaClientExample/KafkaConsumerExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using KafkaNet;
+using KafkaNet.Model;
+
+namespace KafkaConsumerExample
+{
+    public class TestConsumer
+    {
+        private readonly IBrokerRouter _brokerRouter;
+
+        public TestConsumer(IBrokerRouter brokerRouter)
+        {
+            _brokerRouter = brokerRouter;
+        }
+
+        public void StartConsume(string topic)
+        {
+            Console.WriteLine($"Consuming {topic}");
+
+            var consumer = new Consumer(new ConsumerOptions(topic, _brokerRouter));
+
+            foreach (var message in consumer.Consume())
+            {
+                Console.WriteLine("Response: PartitionId:{0}, Offset:{1} Message:{2}",
+                    message.Meta.PartitionId, message.Meta.Offset, Encoding.UTF8.GetString(message.Value));
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kafkaOptions = new KafkaOptions(new Uri("http://192.168.1.35:9092"));
+            var brokerRouter = new BrokerRouter(kafkaOptions);
+            var consumer = new TestConsumer(brokerRouter);
+
+            consumer.StartConsume("TestTopic");
+        }
+    }
+}
---
+++ KafkaClientExample/KafkaProducerExample/Program.cs
@@ -0,0 +1,40 @@
+﻿using KafkaNet;
+using KafkaNet.Model;
+using KafkaNet.Protocol;
+using System;
+
+namespace KafkaProducerExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kafkaOptions = new KafkaOptions(new Uri("http://192.168.1.35:9092"));
+            var brokerRouter = new BrokerRouter(kafkaOptions);
+            var producer = new TestProducer(brokerRouter);
+
+            Console.WriteLine("Send a Message to TestTopic:");
+            while (true)
+            {
+                producer.SendMessageAsync("TestTopic", Console.ReadLine());
+            }
+        }
+    }
+
+    public class TestProducer
+    {
+        private readonly IBrokerRouter _brokerRouter;
+
+        public TestProducer(IBrokerRouter brokerRouter)
+        {
+            _brokerRouter = brokerRouter;
+        }
+
+        public void SendMessageAsync(string topic, string message)
+        {
+            var producer = new Producer(_brokerRouter);
+
+            producer.SendMessageAsync(topic, new[] { new Message(message), }).Wait();
+        }
+    }
+}
